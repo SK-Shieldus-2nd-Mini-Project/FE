9da9764f807d1b3b9db73719cbc823de52494fb0
Seungjun Song <gunmagician@naver.com>
2025-09-05 16:59:10 +0900

    🌟feat: create Router_MainPage_Nav_Footer
    Layout 수정
    CSS 초기화
    개발환경세팅 및 폴더구조 조정

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/Footer.css';
+
+const Footer = () => (
+    <footer>
+        © {new Date().getFullYear()} Oungo. All rights reserved.
+    </footer>
+);
+
+export default Footer;
