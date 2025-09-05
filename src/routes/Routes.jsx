ef2f42a6ffc4d18ff22769524ac1d6f43fcc115a
kbs0621 <jjjustin7531@gmail.com>
2025-09-05 17:13:18 +0900

    login/signup update

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from '../pages/MainPage.jsx'
+import Login from "../pages/Login.jsx"
+import Signup from '../pages/Signup.jsx'
 import Nav from '../components/Nav.jsx'
 import Footer from '../components/Footer.jsx'
 
@@ -13,6 +15,8 @@ export default function Router() {
           path='/'
           element={<MainPage />}
         />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </div>
