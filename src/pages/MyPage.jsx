c7d41840e5a178486466c26c05c3162a19fe218b
Seungjun Song <gunmagician@naver.com>
2025-09-08 13:05:45 +0900

    🌟feat: add GroupApproval + UserManagement
    관리자 페이지 하위 컴포넌트 생성
    - 유저 정보 관리
    - 승인 대기 모임 확인

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -9,7 +9,7 @@ import '../assets/MyPage/MyPage.css';
 // TODO: 실제 로그인된 사용자 정보를 받아와야 함
 // 현재는 'user', 'leader', 'admin' 중 하나로 변경하여 테스트
 const currentUser = {
-  role: 'user', // 'user', 'leader', 'admin'
+  role: 'admin', // 'user', 'leader', 'admin'
   nickname: '운동매니아',
   hasCreatedGroup: true, // 모임을 생성했는지 여부
 };
