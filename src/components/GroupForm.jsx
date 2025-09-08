d82bfe48598c06929651339ca6b054c0fe33c569
kbs0621 <jjjustin7531@gmail.com>
2025-09-08 11:26:42 +0900

    modal update

--- src/components/GroupForm.jsx
+++ src/components/GroupForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import InputField from "../InputField";
+import InputField from "./InputField";
 
 function GroupForm() {
   const [formData, setFormData] = useState({
