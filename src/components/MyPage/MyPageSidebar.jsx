de1e66518b2250a00e15b38b4b147c99ad6ee0a9
kbs0621 <jjjustin7531@gmail.com>
2025-09-10 16:33:50 +0900

    admin/user sidebar allow update

--- src/components/MyPage/MyPageSidebar.jsx
+++ src/components/MyPage/MyPageSidebar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 export default function MyPageSidebar({ user, activeTab, setActiveTab }) {
-  const profileImageUrl = user.profileImage || '/public/mymelody.png';
-
   return (
     <aside className="mypage-sidebar">
       <div className="profile-summary">
-        <img src={profileImageUrl} alt="Profile" className="profile-image" />
+        <img
+          src={user.profileImage || '/public/mymelody.png'}
+          alt="Profile"
+          className="profile-image"
+        />
         <h3>{user.nickname}</h3>
-        <p>{user.role === 'admin' ? '일반 회원' : '관리자'}</p>
+        <p>{user.role === 'ADMIN' ? '관리자' : '일반 회원'}</p>
       </div>
+
       <nav>
         <ul>
-          {/* 공통 메뉴 */}
+          {/* 공통 메뉴: 회원정보 수정 */}
           <li
             className={activeTab === 'editProfile' ? 'active' : ''}
             onClick={() => setActiveTab('editProfile')}
@@ -21,27 +24,29 @@ export default function MyPageSidebar({ user, activeTab, setActiveTab }) {
           </li>
 
           {/* 일반 사용자 메뉴 */}
-          {user.role !== 'admin' && (
-            <li
-              className={activeTab === 'myActivities' ? 'active' : ''}
-              onClick={() => setActiveTab('myActivities')}
-            >
-              내 활동
-            </li>
+          {user.role !== 'ADMIN' && (
+            <>
+              <li
+                className={activeTab === 'myActivities' ? 'active' : ''}
+                onClick={() => setActiveTab('myActivities')}
+              >
+                내 활동
+              </li>
+
+              {/* 모임장 메뉴 */}
+              {user.hasCreatedGroup && (
+                <li
+                  className={activeTab === 'manageRecruitment' ? 'active' : ''}
+                  onClick={() => setActiveTab('manageRecruitment')}
+                >
+                  모집 활동 관리
+                </li>
+              )}
+            </>
           )}
 
-          {/* 모임장 메뉴 */}
-          {user.hasCreatedGroup && user.role !== 'admin' && (
-             <li
-              className={activeTab === 'manageRecruitment' ? 'active' : ''}
-              onClick={() => setActiveTab('manageRecruitment')}
-            >
-              모집 활동 관리
-            </li>
-          )}
-          
           {/* 관리자 메뉴 */}
-          {user.role === 'admin' && (
+          {user.role === 'ADMIN' && (
             <li
               className={activeTab === 'adminPanel' ? 'active' : ''}
               onClick={() => setActiveTab('adminPanel')}
