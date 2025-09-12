91fed01a0cbdd1f4fda271e830c74f4149aea5b1
Seungjun Song <gunmagician@naver.com>
2025-09-12 10:03:54 +0900

    ✨feat: add setupInterceptors for refresh token
    리프레쉬 토큰 발급

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import setupAxiosInterceptors from './api/setupInterceptors.js'
+
+setupAxiosInterceptors(store);
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
